773e559405526975af61956de7c4fdaa41f4e9de
Yahya Mardini <120016906+gityahco@users.noreply.github.com>
2023-12-12 10:06:00 +0300

    idn'tno

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.MYNODE_ENV === 'production';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pathfindingvisualizer/",
-  // base: isProduction ? '/pathfindingvisualizer/' : '/',
+  // base: "/pathfindingvisualizer/",
+  base: isProduction ? '/' : '/pathfindingvisualizer/',
 })
